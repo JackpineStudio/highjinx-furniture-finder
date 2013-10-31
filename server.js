e51100fe5344560e20a20f60d7195e0564f2e12b
Bora Sabuncu <bora@Ekins-MacBook-Pro.local>
2013-10-31 13:09:05 -0400

    Added bootstrap to the project

--- server.js
+++ server.js
@@ -3,7 +3,11 @@
  */
 
 var http = require('http');
-var readFeeds = require('./read_rss');
+var fs = require('fs');
+//var readFeeds = require('./read_rss');
+
+var updateInterval = new Date("October 1, 2013 12:00:00");
+var lastUpdated = new Date("October 31, 2013 12:53:00");
 
 function onRequest(request, response) {
 	var fileName = "index.html";
@@ -25,6 +29,94 @@ function writeToResponse(response, str) {
 }
 
 
+function update() {
+	// Call update on read-rss.js
+	// set lastUpdated
+	lastUpdated = new Date();
+	console.log("Updating...");
+}
+
+function readFromConfigFile() {
+	console.log("Reading from config file");
+	try {
+		var config = require('./server-config.json');
+		console.log(JSON.parse(config));
+	}catch (err) {
+		console.log(err);
+	} 
+	
+}
+
+function changeConfigFile() {
+
+}
+
+function setInterval() {
+
+}
+
+function checkUpdate() {
+	var now = new Date();
+	var now2 = new Date();
+	//now.setHours(15);
+	
+	var nowDay = now.getDate();
+	var nowHours = now.getHours();
+	var nowMinutes = now.getMinutes();
+
+	var lastUpdatedDay = lastUpdated.getDate();
+	var lastUpdatedHours = lastUpdated.getHours();
+	var lastUpdatedMinutes = lastUpdated.getMinutes();
+
+	var dayDiff = nowDay - lastUpdatedDay;
+	var hourDiff = nowHours - lastUpdatedHours;
+	var minuteDiff = nowMinutes - lastUpdatedMinutes;
+
+	var decide = updateInterval.getMinutes();
+	console.log(dayDiff + ":"  + hourDiff + ":" + minuteDiff);
+	if (minuteDiff == decide)
+	{
+		console.log("Time to update");
+		process.exit(code=0);
+	} 
+	
+}
+
+function showMenu() {
+	console.log("Welcome to highjinx furniture finder server");
+	console.log("Commands that can be entered");
+	console.log("1. Manual Update Database");
+	console.log("2. Set interval to update");
+	console.log("3. Restart the server");
+	console.log("4. Exit");
+	rl.question("What would you like to do ? ", function(answer) {
+		executeMenu(answer);
+	});
+}
+
+function executeMenu(num) {
+	if (isNaN(num))
+		console.log("Please enter a number!");
+	else {
+		var number = parseInt(num);
+		switch(number) {
+			case 1: 
+				update();
+				break;
+			case 2:
+				readFromConfigFile();
+				break;
+			case 4: 
+				running = false;
+				process.exit(code=0);
+				break;
+			case 5:
+				checkUpdate();
+				break;
+		}
+	}
+}
+
 var dirName = "./";
 
 //readFeeds.generateFiles();
@@ -32,8 +124,15 @@ var dirName = "./";
 var connect = require('connect');
 
 var path = require('path');
+var readline = require('readline');
+
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
 var app  = connect()
+	
 	.use(connect.static(dirName + "index.html"))
 	.use(function(request, response) { 
 		
@@ -67,3 +166,6 @@ var app  = connect()
 		});		
 	});
 http.createServer(app).listen(8080);
+var running = true;
+showMenu();
+
